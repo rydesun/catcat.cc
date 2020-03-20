8f8846b191ec67658866f49bdd965d150087d552
rydesun <rydesun@gmail.com>
2020-03-20 19:57:49 +0800

    lazyload comments

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -1,23 +1,37 @@
+var detectDark = window.matchMedia('(prefers-color-scheme: dark)')
+
 var remark_config = {
   host: "https://comments.srv.2cat.cc",
   site_id: '2cat.cc',
   components: ['embed'],
 };
-(function(c) {
-  for(var i = 0; i < c.length; i++){
+
+function loadRemark42() {
+  var commentComponents = remark_config.components || ['embed'];
+  for(component of commentComponents) {
     var d = document, s = d.createElement('script');
-    s.src = remark_config.host + '/web/' +c[i] +'.js';
+    s.src = remark_config.host + '/web/' + component +'.js';
     s.defer = true;
     (d.head || d.body).appendChild(s);
   }
-})(remark_config.components || ['embed']);
-var remarkTheme ="";
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", function(e) {
-  if (e.matches) {
-    remarkTheme = "dark"
-    window.REMARK42.changeTheme('dark')
-  } else {
-    remarkTheme = "light"
-    window.REMARK42.changeTheme('light')
-  }
+}
+
+setTimeout(() => {
+  var commentObserver = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+      loadRemark42();
+      commentObserver.disconnect();
+    }
+  }, { threshold: [0] });
+  commentObserver.observe(document.getElementById('remark42'));
+}, 1);
+
+// FIXME: May work after remark42 loaded completely
+if (detectDark.matches) {
+  window.REMARK42.changeTheme('dark');
+}
+
+detectDark.addEventListener("change", function(e) {
+  var theme = e.matches ? 'dark' : 'light';
+  window.REMARK42.changeTheme(theme);
 });
