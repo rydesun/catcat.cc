a743fd522b0acaacfe360eb2e80e87d7b657be2a
rydesun <rydesun@gmail.com>
2020-06-25 18:29:19 +0800

    Add scroll to top button

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -24,3 +24,40 @@ function toggleFixTop() {
     menu.classList.add('fix-top');
   }
 }
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
+
+var scrollToTopTimeout;
+
+function showScrollToTop() {
+  let button = document.getElementById("scroll");
+  button.classList.add('scrolling');
+  if (scrollToTopTimeout) {
+    clearTimeout(scrollToTopTimeout);
+  }
+  scrollToTopTimeout = setTimeout(function() {
+    button.classList.remove('scrolling');
+  }, 2000);
+}
+
+var positionY = 0;
+var ticking = false;
+window.addEventListener('scroll', function(e) {
+  let offsetY = window.scrollY - positionY;
+  positionY = window.scrollY;
+  if (window. innerWidth > 900) {
+    return;
+  }
+  if (offsetY > -100) {
+    return;
+  }
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      showScrollToTop();
+      ticking = false;
+    });
+  }
+  ticking = true;
+});
