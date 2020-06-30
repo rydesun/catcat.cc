cc2a9ea1a56dc564ed5f3ca8295924728cd7f3c4
rydesun <rydesun@gmail.com>
2020-06-30 20:30:06 +0800

    Refactor css

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ code_dark_theme=dracula
 old_font=$(ls public/fonts)
 
 mkdir -p ./assets/css/vendor/
+cp ./themes/hugo-notepadium/assets/css/dark-style.css ./assets/css/vendor/dark-style.scss
 cp ./themes/hugo-notepadium/assets/css/chroma/${code_dark_theme}.css ./assets/css/vendor/${code_dark_theme}.scss
 rm -r ./assets/js/build
 yarn run build
