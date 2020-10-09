3751d08bb37595e3f216e051e553b4d6502c982f
rydesun <rydesun@gmail.com>
2020-10-09 12:52:40 +0800

    Update warning banner: use google.cn

--- src/warnIE.js
+++ src/warnIE.js
@@ -9,7 +9,7 @@ if (checkIsIE()) {
     box.innerHTML = [
         '<p>正在使用IE浏览器？IE是网页的噩梦，是混乱的祸根。</p>',
         '<p>提升网页体验，推荐立刻换用 <a href="https://www.mozilla.org/zh-CN/firefox/new/" rel="noopener nofollow noreferrer">Firefox浏览器</a> 或者',
-        '<a href="https://www.google.com/intl/zh-CN/chrome/" rel="noopener nofollow noreferrer">Chrome浏览器</a></p>',
+        '<a href="https://www.google.cn/intl/zh-CN/chrome/" rel="noopener nofollow noreferrer">Chrome浏览器</a></p>',
     ].join('\n');
     var body = document.getElementsByTagName('body')[0];
     var content = document.getElementsByClassName('base-body')[0];
